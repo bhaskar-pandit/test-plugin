9430c24d0182b4ca0d3b4efaed8f62fa0ba5264b
bhaskar-pandit <67310728+bhaskar-pandit@users.noreply.github.com>
2024-03-23 01:38:18 +0530

    Update smashing-plugin.php

--- smashing-plugin.php
+++ smashing-plugin.php
@@ -2,7 +2,7 @@
 /*
 	Plugin Name: Smashing Plugin
 	Description: This is for updating your Wordpress plugin.
-	Version: 1.0.3
+	Version: 1.0.4
 	Author: Matthew Ray
 	Author URI: http://www.matthewray.com
 */
